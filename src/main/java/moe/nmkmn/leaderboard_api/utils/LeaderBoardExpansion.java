69a6bc0f35aeb101b562adefc56189547309f635
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-05-05 14:55:11 +0900

    Fix: switch文に変更

--- src/main/java/moe/nmkmn/leaderboard_api/utils/LeaderBoardExpansion.java
+++ src/main/java/moe/nmkmn/leaderboard_api/utils/LeaderBoardExpansion.java
@@ -71,161 +71,165 @@ public class LeaderBoardExpansion extends PlaceholderExpansion {
         String[] params_split = params.split("_");
 
         try {
-            if (params_split[0].equals("block")) {
-                if (params_split[1].equals("break")) {
-                    if (params_split[2].equals("rank")) {
+            switch (params_split[0]) {
+                case "block" -> {
+                    if (params_split[1].equals("break")) {
+                        if (params_split[2].equals("rank")) {
+                            List<String> names = new ArrayList<>();
+                            List<Long> values = new ArrayList<>();
+
+                            try {
+                                PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(blockBreak,64,0) DESC LIMIT 10");
+                                ResultSet resultSet = statement.executeQuery();
+
+                                while (resultSet.next()) {
+                                    names.add(Bukkit.getOfflinePlayer(UUID.fromString(resultSet.getString("uuid"))).getName());
+                                    values.add(resultSet.getLong("blockBreak"));
+                                }
+                            } catch (SQLException e) {
+                                return null;
+                            }
+
+                            if (params_split[4].equals("name")) {
+                                if (names.size() > (Integer.parseInt(params_split[3]) - 1)) {
+                                    return names.get(Integer.parseInt(params_split[3]) - 1);
+                                } else {
+                                    return "None";
+                                }
+                            } else if (params_split[4].equals("value")) {
+                                if (values.size() > (Integer.parseInt(params_split[3]) - 1)) {
+                                    return values.get(Integer.parseInt(params_split[3]) - 1).toString();
+                                } else {
+                                    return "NaN";
+                                }
+                            }
+                        } else if (params_split[2].equals("me")) {
+                            PlayerDB playerDB = new PlayerDB();
+
+                            PlayerModel playerModel = playerDB.findByUUID(database.connection(), player.getUniqueId().toString());
+
+                            return String.valueOf(playerModel.getBlockBreak());
+                        }
+                    } else if (params_split[1].equals("place")) {
+                        if (params_split[2].equals("rank")) {
+                            List<String> names = new ArrayList<>();
+                            List<Long> values = new ArrayList<>();
+
+                            try {
+                                PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(blockPlace,64,0) DESC LIMIT 10");
+                                ResultSet resultSet = statement.executeQuery();
+
+                                while (resultSet.next()) {
+                                    names.add(Bukkit.getOfflinePlayer(UUID.fromString(resultSet.getString("uuid"))).getName());
+                                    values.add(resultSet.getLong("blockPlace"));
+                                }
+                            } catch (SQLException e) {
+                                return null;
+                            }
+
+                            if (params_split[4].equals("name")) {
+                                if (names.size() > (Integer.parseInt(params_split[3]) - 1)) {
+                                    return names.get(Integer.parseInt(params_split[3]) - 1);
+                                } else {
+                                    return "None";
+                                }
+                            } else if (params_split[4].equals("value")) {
+                                if (values.size() > (Integer.parseInt(params_split[3]) - 1)) {
+                                    return values.get(Integer.parseInt(params_split[3]) - 1).toString();
+                                } else {
+                                    return "NaN";
+                                }
+                            }
+                        } else if (params_split[2].equals("me")) {
+                            PlayerDB playerDB = new PlayerDB();
+
+                            PlayerModel playerModel = playerDB.findByUUID(database.connection(), player.getUniqueId().toString());
+
+                            return String.valueOf(playerModel.getBlockPlace());
+                        }
+                    }
+                }
+                case "balance" -> {
+                    if (params_split[1].equals("rank")) {
                         List<String> names = new ArrayList<>();
-                        List<Long> values = new ArrayList<>();
+                        List<Double> values = new ArrayList<>();
 
                         try {
-                            PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(blockBreak,64,0) DESC LIMIT 10");
+                            PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(balance,64,0) DESC LIMIT 10");
                             ResultSet resultSet = statement.executeQuery();
 
                             while (resultSet.next()) {
                                 names.add(Bukkit.getOfflinePlayer(UUID.fromString(resultSet.getString("uuid"))).getName());
-                                values.add(resultSet.getLong("blockBreak"));
+                                values.add(resultSet.getDouble("balance"));
                             }
                         } catch (SQLException e) {
                             return null;
                         }
 
-                        if (params_split[4].equals("name")) {
-                            if (names.size() > (Integer.parseInt(params_split[3]) - 1)) {
-                                return names.get(Integer.parseInt(params_split[3]) - 1);
+                        if (params_split[3].equals("name")) {
+                            if (names.size() > (Integer.parseInt(params_split[2]) - 1)) {
+                                return names.get(Integer.parseInt(params_split[2]) - 1);
                             } else {
                                 return "None";
                             }
-                        } else if (params_split[4].equals("value")) {
-                            if (values.size() > (Integer.parseInt(params_split[3]) - 1)) {
-                                return values.get(Integer.parseInt(params_split[3]) - 1).toString();
+                        } else if (params_split[3].equals("value")) {
+                            if (values.size() > (Integer.parseInt(params_split[2]) - 1)) {
+                                return String.valueOf(Math.round(values.get(Integer.parseInt(params_split[2]) - 1)));
                             } else {
                                 return "NaN";
                             }
                         }
-                    } else if (params_split[2].equals("me")) {
+                    } else if (params_split[1].equals("me")) {
                         PlayerDB playerDB = new PlayerDB();
 
                         PlayerModel playerModel = playerDB.findByUUID(database.connection(), player.getUniqueId().toString());
 
-                        return String.valueOf(playerModel.getBlockBreak());
+                        return String.valueOf(playerModel.getBalance());
                     }
-                } else if (params_split[1].equals("place")) {
-                    if (params_split[2].equals("rank")) {
+                }
+                case "playtime" -> {
+                    if (params_split[1].equals("rank")) {
                         List<String> names = new ArrayList<>();
                         List<Long> values = new ArrayList<>();
 
                         try {
-                            PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(blockPlace,64,0) DESC LIMIT 10");
+                            PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(playTime,64,0) DESC LIMIT 10");
                             ResultSet resultSet = statement.executeQuery();
 
                             while (resultSet.next()) {
                                 names.add(Bukkit.getOfflinePlayer(UUID.fromString(resultSet.getString("uuid"))).getName());
-                                values.add(resultSet.getLong("blockPlace"));
+                                values.add(resultSet.getLong("playTime"));
                             }
                         } catch (SQLException e) {
                             return null;
                         }
 
-                        if (params_split[4].equals("name")) {
-                            if (names.size() > (Integer.parseInt(params_split[3]) - 1)) {
-                                return names.get(Integer.parseInt(params_split[3]) - 1);
+                        if (params_split[3].equals("name")) {
+                            if (names.size() > (Integer.parseInt(params_split[2]) - 1)) {
+                                return names.get(Integer.parseInt(params_split[2]) - 1);
                             } else {
                                 return "None";
                             }
-                        } else if (params_split[4].equals("value")) {
-                            if (values.size() > (Integer.parseInt(params_split[3]) - 1)) {
-                                return values.get(Integer.parseInt(params_split[3]) - 1).toString();
+                        } else if (params_split[3].equals("value")) {
+                            if (values.size() > (Integer.parseInt(params_split[2]) - 1)) {
+                                if (params_split[4].equals("raw")) {
+                                    return String.valueOf(values.get(Integer.parseInt(params_split[2]) - 1));
+                                } else {
+                                    return formatTime(values.get(Integer.parseInt(params_split[2]) - 1));
+                                }
                             } else {
                                 return "NaN";
                             }
                         }
-                    } else if (params_split[2].equals("me")) {
+                    } else if (params_split[1].equals("me")) {
                         PlayerDB playerDB = new PlayerDB();
-
                         PlayerModel playerModel = playerDB.findByUUID(database.connection(), player.getUniqueId().toString());
 
-                        return String.valueOf(playerModel.getBlockPlace());
-                    }
-                }
-            } else if (params_split[0].equals("balance")) {
-                if (params_split[1].equals("rank")) {
-                    List<String> names = new ArrayList<>();
-                    List<Double> values = new ArrayList<>();
-
-                    try {
-                        PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(balance,64,0) DESC LIMIT 10");
-                        ResultSet resultSet = statement.executeQuery();
-
-                        while (resultSet.next()) {
-                            names.add(Bukkit.getOfflinePlayer(UUID.fromString(resultSet.getString("uuid"))).getName());
-                            values.add(resultSet.getDouble("balance"));
-                        }
-                    } catch (SQLException e) {
-                        return null;
-                    }
-
-                    if (params_split[3].equals("name")) {
-                        if (names.size() > (Integer.parseInt(params_split[2]) - 1)) {
-                            return names.get(Integer.parseInt(params_split[2]) - 1);
+                        if (params_split[2].equals("raw")) {
+                            return String.valueOf(playerModel.getPlayTime());
                         } else {
-                            return "None";
+                            return formatTime(playerModel.getPlayTime());
                         }
-                    } else if (params_split[3].equals("value")) {
-                        if (values.size() > (Integer.parseInt(params_split[2]) - 1)) {
-                            return String.valueOf(Math.round(values.get(Integer.parseInt(params_split[2]) - 1)));
-                        } else {
-                            return "NaN";
-                        }
-                    }
-                } else if (params_split[1].equals("me")) {
-                    PlayerDB playerDB = new PlayerDB();
-
-                    PlayerModel playerModel = playerDB.findByUUID(database.connection(), player.getUniqueId().toString());
-
-                    return String.valueOf(playerModel.getBalance());
-                }
-            } else if (params_split[0].equals("playtime")) {
-                if (params_split[1].equals("rank")) {
-                    List<String> names = new ArrayList<>();
-                    List<Long> values = new ArrayList<>();
-
-                    try {
-                        PreparedStatement statement = database.connection().prepareStatement("SELECT * FROM Player ORDER BY LPAD(playTime,64,0) DESC LIMIT 10");
-                        ResultSet resultSet = statement.executeQuery();
-
-                        while (resultSet.next()) {
-                            names.add(Bukkit.getOfflinePlayer(UUID.fromString(resultSet.getString("uuid"))).getName());
-                            values.add(resultSet.getLong("playTime"));
-                        }
-                    } catch (SQLException e) {
-                        return null;
-                    }
-
-                    if (params_split[3].equals("name")) {
-                        if (names.size() > (Integer.parseInt(params_split[2]) - 1)) {
-                            return names.get(Integer.parseInt(params_split[2]) - 1);
-                        } else {
-                            return "None";
-                        }
-                    } else if (params_split[3].equals("value")) {
-                        if (values.size() > (Integer.parseInt(params_split[2]) - 1)) {
-                            if (params_split[4].equals("raw")) {
-                                return String.valueOf(values.get(Integer.parseInt(params_split[2]) - 1));
-                            } else {
-                                return formatTime(values.get(Integer.parseInt(params_split[2]) - 1));
-                            }
-                        } else {
-                            return "NaN";
-                        }
-                    }
-                } else if (params_split[1].equals("me")) {
-                    PlayerDB playerDB = new PlayerDB();
-                    PlayerModel playerModel = playerDB.findByUUID(database.connection(), player.getUniqueId().toString());
-
-                    if (params_split[2].equals("raw")) {
-                        return String.valueOf(playerModel.getPlayTime());
-                    } else {
-                        return formatTime(playerModel.getPlayTime());
                     }
                 }
             }
